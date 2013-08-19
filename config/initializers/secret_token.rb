2d38c3cd434a494ffc7dcf2e957e5949a09bc245
jyli7 <jyl702@gmail.com>
2013-08-19 02:48:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qualia::Application.config.secret_key_base = '08eaeb57a0a0102cfbedbb134f5c094811bd2b9d0b578475ec740a0e965767124e08cc0955b42aaeb2081753461197800c6262a1355886a29b18b27005859d23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Qualia::Application.config.session_store :cookie_store, key: '_qualia_session'
