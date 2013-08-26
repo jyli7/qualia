71b43e792241f794b2d8f5a21b681cbd388e9ed0
jyli7 <jyl702@gmail.com>
2013-08-26 16:12:02 -0400

    add sunil back

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,23 @@
 $(function () {
+	$('.photo-port').on('mouseenter', function () {
+		$(this).addClass('hovered');
+	});
+
+	$('.photo-port').on('mouseleave', function () {
+		$(this).removeClass('hovered');
+	});
+
 	$('.photo-port').each(function () {
 		var name = $(this).data('name');
 		var desc = $(this).data('desc');
+
 		$(this).tooltip({
 			content: "<h3>" + name + "</h3>" + "<p>" + desc + "</p>",
 			position: {
-				at: "bottom-145",
+				at: "bottom-150",
 				my: "bottom",
 				collision: "none"
 			}
 		});
 	});
-	
 });
