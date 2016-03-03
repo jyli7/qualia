fec4d70b0a6b18be129aad0541de9beaf87235f4
jyli7 <jyl702@gmail.com>
2016-03-03 08:13:29 -0500

    slight changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,11 +12,6 @@ class StaticPagesController < ApplicationController
   			desc: "Pedro specializes in Ruby, Python, Javascript, and php and has deep experience with a host of web frameworks. Before co-founding Qualia, he was the sole developer at Driver Digital and studied philosophy at Harvard.",
         url: "http://www.github.com/pvmoura"
   		},
-  		{ name: "Alyssa Frazee",
-  			image: "alyssa.jpg",
-  			desc: "Alyssa works in data science, primarily programming in R and Python.  She studied math at St. Olaf College and is currently working in the area of computational biology while finishing her PhD in biostatistics at Johns Hopkins University.",
-        url: "http://www.alyssafrazee.com"
-  		},
       { name: "Stephen Dagadakis",
         image: "stephen.png",
         desc: "Stephen does web development in Washington, D.C.  Previously, he programmed a PHP-based geospatial social network, did virtual reality research, and served as Deputy Executive Director of a leadership office in the U.S. Congress.",
