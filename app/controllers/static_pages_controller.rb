9306181ef8921327a2f40bb45710e53269810aeb
jyli7 <jyl702@gmail.com>
2016-03-02 19:01:54 -0500

    web dev

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,11 +7,6 @@ class StaticPagesController < ApplicationController
   			desc: "Before co-founding Qualia, Jimmy worked as a Ruby and Javascript developer at Codecademy in NYC and Pivotal Labs in SF. He studied neurobiology at Harvard and is finishing his J.D. at Yale.",
         url: "http://www.jylcreations.com"
   		},
-  		{ name: "Lauren Sperber",
-  			image: "lauren.jpg",
-  			desc: "Lauren is a front-end developer specializing in JavaScript, CSS, and Rails. She previously worked as a product manager at Aol/Patch and Outside.in and studied literature at Georgetown and NYU.",
-        url: "http://www.laurensperber.com"
-  		},
   		{ name: "Pedro Moura",
   			image: "pedro.jpg",
   			desc: "Pedro specializes in Ruby, Python, Javascript, and php and has deep experience with a host of web frameworks. Before co-founding Qualia, he was the sole developer at Driver Digital and studied philosophy at Harvard.",
@@ -21,11 +16,6 @@ class StaticPagesController < ApplicationController
   			image: "alyssa.jpg",
   			desc: "Alyssa works in data science, primarily programming in R and Python.  She studied math at St. Olaf College and is currently working in the area of computational biology while finishing her PhD in biostatistics at Johns Hopkins University.",
         url: "http://www.alyssafrazee.com"
-  		},
-  		{ name: "Sunil Abraham",
-  			image: "sunil.png",
-  			desc: "Sunil worked on machine learning and statistics at the NSA and as a Fulbright Scholar at the University of Waterloo. Now he works on data science in R and backend development in Clojure and Python. He studied math at Oxford and statistics at Cambridge.",
-        url: "http://www.github.com/sabraham"
   		},
       { name: "Stephen Dagadakis",
         image: "stephen.png",
